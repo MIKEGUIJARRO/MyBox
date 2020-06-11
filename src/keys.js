c0082060bfdb4fa72d7c5ef4f3804f5640c1c194
Mike Guijarro <a01612042@itesm.mx>
2020-06-10 22:24:44 -0500

    Update info 2

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
-        host: 'localhost',
+        host: 'us-cdbr-east-05.cleardb.net',
         user: 'b3a45ee63ed8ef',
         password: '70b4bbbd',
-        database: 'database_packages',
+        database: 'heroku_c36a522c44878e9',
         insecureAuth: true,
         username: undefined
     }
